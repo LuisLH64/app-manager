81628550cccd7eef33f1fcca22a3dcdd13a14f33
Luís H. <luishp2001@gmail.com>
2024-10-14 15:23:01 -0300

    Update URL
    -EN: Added the new URLs that will be used in the controllers
    -PT-BR: Adicionado as novas URLs que serão usadas nos controllers

--- src/main/java/com/app_manager/util/Url.java
+++ src/main/java/com/app_manager/util/Url.java
@@ -7,4 +7,10 @@ public abstract class Url {
     public static final String FIND_BY_NAME = "/find/by-name";
     public static final String FIND_BY_USERNAME = "/find/by-username";
     public static final String FIND_BY_EMAIL = "/find/by-email";
+
+    public static final String INSERT = "/insert";
+    public static final String UPDATE = "/update";
+    public static final String UPDATE_BY_ID = "/update/{id}";
+    public static final String DELETE = "/delete";
+    public static final String DELETE_BY_ID = "/delete/{id}";
 }
