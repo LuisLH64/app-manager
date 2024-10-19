6775285f4734ef19edd5451aa170ff45184ddc2c
Luís H. <luishp2001@gmail.com>
2024-10-19 15:08:08 -0300

    Update UserDTO.java
    EN:
    -Converted the normal class into a Record, to fit a standard for DTOs.
    
    PT-BR:
    -Convertido a classe normal em um Record, para ficar em um padrão para DTOs.

--- src/main/java/com/app_manager/user/UserDTO.java
+++ src/main/java/com/app_manager/user/UserDTO.java
@@ -1,17 +1,6 @@
 package com.app_manager.user;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class UserDTO {
-    
-    private String name;
-    private String username;
-    private String email;
-    private String password;
-    private String role;
+public record UserDTO(String name, String username, String email, String password, String role) {
 
     public UserEntity toUserEntity() {
 
