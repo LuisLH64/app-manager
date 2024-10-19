e6f6d66718d81397527dc951105419449704643a
Luís H. <luishp2001@gmail.com>
2024-10-19 15:08:50 -0300

    Update UserEntity.java
    EN:
    -Removed unnecessary imports.
    
    PT-BR:
    -Retirado os imports desnecessários.

--- src/main/java/com/app_manager/user/UserEntity.java
+++ src/main/java/com/app_manager/user/UserEntity.java
@@ -7,8 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
