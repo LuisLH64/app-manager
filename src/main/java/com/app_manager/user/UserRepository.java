f7df49a82b4b824bc3e1a6e10decb8610d5d6bc6
Luís H. <luishp2001@gmail.com>
2024-10-19 15:09:49 -0300

    Update UserRepository.java
    EN:
    -Set the entity return option as Optional.
    
    PT-BR:
    -Colocado a opção de retorno da entidade como Optional.

--- src/main/java/com/app_manager/user/UserRepository.java
+++ src/main/java/com/app_manager/user/UserRepository.java
@@ -3,13 +3,14 @@ package com.app_manager.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByUsername(String username);
 
-    UserEntity findByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
 
     List<UserEntity> findByName(String name);
 }
